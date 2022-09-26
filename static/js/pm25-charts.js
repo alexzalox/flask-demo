903e26363bd9f56cff76925b0860d03a99ad75dd
Alex <alexzalox@gmail.com>
2022-09-26 19:40:49 +0800

    測試echart其他圖形效果

--- static/js/pm25-charts.js
+++ static/js/pm25-charts.js
@@ -1,48 +1,160 @@
 let chart = echarts.init(document.querySelector("#main"));
 
-drawPM25();
+$(document).ready(() => {
+    drawPM25();
+});
+
+
 // 呼叫ajax(jQuery)
 function drawPM25() {
-
+    chart.showLoading();
     $.ajax(
         {
             url: "/pm25-json",
             type: "GET",
             dataType: "json",
             success: (data) => {
+                chart.hideLoading();
                 console.log(data);
+                // drawPM25Chart(data);
+                drawPM25Chart2(data);
             },
-            errer: () => {
+            error: () => {
+                chart.hideLoading();
                 alert("讀取資料失敗!");
             }
         }
     );
+}
+
+function drawPM25Chart2(data2) {
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    // prettier-ignore
+    let dataAxis = data2["site"];
+
+    // prettier-ignore
+    let data = data2["pm25"];
+    let yMax = 500;
+    let dataShadow = [];
+    for (let i = 0; i < data.length; i++) {
+        dataShadow.push(yMax);
+    }
+    option = {
+        title: {
+            text: '特性示例：渐变色 阴影 点击缩放',
+            subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
+        },
+        xAxis: {
+            data: dataAxis,
+            axisLabel: {
+                inside: true,
+                color: '#fff'
+            },
+            axisTick: {
+                show: false
+            },
+            axisLine: {
+                show: false
+            },
+            z: 10
+        },
+        yAxis: {
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+            axisLabel: {
+                color: '#999'
+            }
+        },
+        dataZoom: [
+            {
+                type: 'inside'
+            }
+        ],
+        series: [
+            {
+                type: 'bar',
+                showBackground: true,
+                itemStyle: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                        { offset: 0, color: '#83bff6' },
+                        { offset: 0.5, color: '#188df0' },
+                        { offset: 1, color: '#188df0' }
+                    ])
+                },
+                emphasis: {
+                    itemStyle: {
+                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                            { offset: 0, color: '#2378f7' },
+                            { offset: 0.7, color: '#2378f7' },
+                            { offset: 1, color: '#83bff6' }
+                        ])
+                    }
+                },
+                data: data
+            }
+        ]
+    };
+    // Enable data zoom when user click bar.
+    const zoomSize = 6;
+    myChart.on('click', function (params) {
+        console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
+        myChart.dispatchAction({
+            type: 'dataZoom',
+            startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
+            endValue:
+                dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
+        });
+    });
+
+    option && myChart.setOption(option);
+
+}
 
 
+function drawPM25Chart(data) {
     // 指定图表的配置项和数据
     let option = {
         title: {
-            text: 'ECharts 入门示例'
+            text: 'PM2.5 全圖數據圖'
         },
-        tooltip: {},
+        toolbox: {
+            show: true,
+            orient: 'vertical',
+            left: 'left',
+            top: 'center',
+            feature: {
+                magicType: { show: true, type: ['line', 'bar', 'tiled'] },
+                restore: { show: true },
+                saveAsImage: { show: true }
+            }
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
         legend: {
-            data: ['销量']
+            data: ['PM2.5']
         },
         xAxis: {
-            data: ['Asus NoteBook', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+            data: data["site"]
         },
         yAxis: {},
         series: [
             {
-                name: '销量',
+                name: 'PM2.5',
                 type: 'bar',
-                data: [100, 20, 36, 10, 10, 20]
+                data: data["pm25"]
             }
         ]
     };
 
-
-
-
     chart.setOption(option);
 }
+
+
